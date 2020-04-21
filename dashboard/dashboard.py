c0c6b6ba2acc5374c345e254d643b6ff5e022fec
mikhail <mr.ycrochick@mail.ry>
2020-04-21 22:49:15 +0300

    Add new bar with statistics

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,7 @@ from django.db.models.functions import TruncMonth, TruncDay
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.template.loader import render_to_string
-from innovation.models import ToolsAndInnovations
+from innovation.models import ToolsAndInnovations, Metadata
 from .base import Dashboard
 
 User = get_user_model()
@@ -56,18 +56,27 @@ def registration_stats(request):
 
 
 def login_stats(request):
-    stats = [
-        {'label': user['label'].strftime('%Y-%m-%d'),
-         'count': user['count']}
-        for user in User.objects
-        .filter(last_login__isnull=False)
-        .annotate(label=TruncDay('last_login'))
-        .values('label')
-        .annotate(count=Count('id'))[:90]
-    ]
+    stats = ToolsAndInnovations.objects.values('prime_phase_number').annotate(dcount=Count('prime_phase_number'))
+    statsWithPhase = [stage for stage in stats if stage['prime_phase_number'].isdigit()]
+    stats = [stage for stage in stats if not stage['prime_phase_number'].isdigit()]
+    statswithphase = sorted(statsWithPhase, key=lambda tup: int(tup['prime_phase_number']))
+    statsDict = {}
+    for st in statswithphase:
+        try:
+            metadata = Metadata.objects.get(research_phase_number=st['prime_phase_number'])
+            statsDict[metadata.research_phases7] =statsDict.get(metadata.research_phases7, 0)  + st['dcount']
+        except Metadata.DoesNotExist:
+            statsDict['NoInformation'] =statsDict.get('NoInformation', 0)  + st['dcount']
+    for st in stats:
+        statsDict['NoInformation'] = statsDict.get('NoInformation', 0) + st['dcount']
+    # for st in statswithphase:
     return render_to_string('dashboard/stats.html', {
-        'title': 'User login statistics',
-        'stats': stats
+        'title': 'Research Phase Stages statistics',
+        'stats': [
+            {'label': phase,
+             'count': count}
+            for phase, count in statsDict.items()
+        ]
     })
 
 
